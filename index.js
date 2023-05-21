c4b61065f3c6e8c184166ac8a7e4d8460789d337
idhayadhulla <126640408+idhayadhulla@users.noreply.github.com>
2023-05-21 09:07:47 -0700

    Update index.js
    lines added

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function sound(){
-var snd=new Audio('car-sound.mp3')
-snd.play()
-}
-
 function start(){
     var snd=new Audio('engine-start.mp3')
     snd.play()
     }
-
+function ride(){
+var snd=new Audio('car-sound.mp3')
+snd.play()
+}
